e7a2ce85ccdb1ed35b27421ce4d36d98050d1d4a
hendezmohamed <116646083+hendezmohamed@users.noreply.github.com>
2022-11-13 23:50:10 +0100

    Add files via upload

---
+++ produits/src/main/java/com/hendez/produits/entities/Produit.java
@@ -0,0 +1,84 @@
+package com.hendez.produits.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+@Entity
+public class Produit {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long idProduit;
+	private String nomProduit;
+	private double prixProduit;
+	private Date dateCreation;
+	
+	@ManyToOne
+	private Categorie categorie;
+	
+	
+	
+	public Produit() {
+		super();
+		
+	}
+	
+	
+	public Produit(String nomProduit, double prixProduit, Date dateCreation) {
+		super();
+		this.nomProduit = nomProduit;
+		this.prixProduit = prixProduit;
+		this.dateCreation = dateCreation;
+	}
+
+
+	public long getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(long idProduit) {
+		this.idProduit = idProduit;
+	}
+	public String getNomProduit() {
+		return nomProduit;
+	}
+	public void setNomProduit(String nomProduit) {
+		this.nomProduit = nomProduit;
+	}
+	public double getPrixProduit() {
+		return prixProduit;
+	}
+	public void setPrixProduit(double prixProduit) {
+		this.prixProduit = prixProduit;
+	}
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Produit [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", prixProduit=" + prixProduit
+				+ ", dateCreation=" + dateCreation + "]";
+	}
+
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+	
+	
+
+	
+}
